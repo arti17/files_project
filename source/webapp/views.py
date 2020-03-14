eec39ee688b23392aa2932590d1924cd83eee20b
Artem Tkachuk <arti17900@gmail.com>
2020-03-14 13:49:33 +0600

    Добавлена пагинация и поиск по названию на главной странице.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import User
+from django.db.models import Q
 from django.urls import reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
+from webapp.forms import SearchForm
 from webapp.models import File
 
 
@@ -9,6 +12,34 @@ class HomeView(ListView):
     template_name = 'home.html'
     model = File
     ordering = '-created_at'
+    paginate_by = 10
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=None, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(name__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class FileDetailView(DetailView):
