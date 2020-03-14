931628ba5afc4fbe13b6c091fb5a1adaa5d107ba
Artem Tkachuk <arti17900@gmail.com>
2020-03-14 11:01:05 +0600

    Создана модель File.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(User, null=True, blank=True, related_name='files', on_delete=models.CASCADE, verbose_name='Автор')
